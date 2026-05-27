60b8d9458b173d75889d3f6e98adfb29ce03a2dc
divyank420 <divyank.kabra@technostacks.com>
2026-05-27 17:13:57 +0530

    website changes

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,8 @@ use Illuminate\Support\Facades\Http;
 */
 
 Route::get('/', function () {
+    return view('welcome');
     return view('SEO.index');
-    return view('website.index');
 });
 Route::get('/rms-code', function () {
     return view('RmCode');
@@ -61,42 +61,42 @@ Route::prefix('agent')->group(function () {
 });
 
 
-Route::get('/test-fcm', function () {
+// Route::get('/test-fcm', function () {
 
-    $deviceToken = "c2BvY7lXQ2ST_LS_spovTY:APA91bGuRyeo2f5iDwcc7C-OXx9gdrIalUJEAY3NwVOD34RUXU6ee0bRBzAcJZH9Ljp08SBZqBUAp1UqzPmBb32o7JNaBDfvy-f1UIBMIViurJiKWLLhvAE";
+//     $deviceToken = "c2BvY7lXQ2ST_LS_spovTY:APA91bGuRyeo2f5iDwcc7C-OXx9gdrIalUJEAY3NwVOD34RUXU6ee0bRBzAcJZH9Ljp08SBZqBUAp1UqzPmBb32o7JNaBDfvy-f1UIBMIViurJiKWLLhvAE";
 
-    $credentials = json_decode(
-        file_get_contents(storage_path('app/firebase/firebase.json')),
-        true
-    );
+//     $credentials = json_decode(
+//         file_get_contents(storage_path('app/firebase/firebase.json')),
+//         true
+//     );
 
-    $client = new \Google\Auth\Credentials\ServiceAccountCredentials(
-        "https://www.googleapis.com/auth/firebase.messaging",
-        $credentials
-    );
+//     $client = new \Google\Auth\Credentials\ServiceAccountCredentials(
+//         "https://www.googleapis.com/auth/firebase.messaging",
+//         $credentials
+//     );
 
-    $token = $client->fetchAuthToken();
-    $accessToken = $token['access_token'];
+//     $token = $client->fetchAuthToken();
+//     $accessToken = $token['access_token'];
 
-    $projectId = $credentials['project_id'];
+//     $projectId = $credentials['project_id'];
 
-    $response = Http::withToken($accessToken)
-        ->post("https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send", [
-            "message" => [
-                "token" => $deviceToken,
-                "notification" => [
-                    "title" => "Laravel Test 🚀",
-                    "body" => "Notification is working!",
-                    "android_channel_id" => "default_channel_id",
-                ],
-                "android" => [
-                    "notification" => [
-                        "sound" => "default",
-                        "default_sound" => true
-                    ]
-                ]
-            ]
-        ]);
+//     $response = Http::withToken($accessToken)
+//         ->post("https://fcm.googleapis.com/v1/projects/{$projectId}/messages:send", [
+//             "message" => [
+//                 "token" => $deviceToken,
+//                 "notification" => [
+//                     "title" => "Laravel Test 🚀",
+//                     "body" => "Notification is working!",
+//                     "android_channel_id" => "default_channel_id",
+//                 ],
+//                 "android" => [
+//                     "notification" => [
+//                         "sound" => "default",
+//                         "default_sound" => true
+//                     ]
+//                 ]
+//             ]
+//         ]);
 
-    return $response->json();
-});
+//     return $response->json();
+// });
